7c47b2999e9e9395cb266035fb3e9930f8b4c681
edmar.james <edmarjamesbautista@gmail.com>
2023-03-06 19:31:24 +0800

    Add comments on JS file of Lesson 13 to 18

--- lesson13-18/index.js
+++ lesson13-18/index.js
@@ -1,7 +1,19 @@
+// register the 'ngRoute' as dependency to our module for us to use the '$routeProvider'
 const myApp = angular.module('myApp', ['ngRoute']);
 
+
+// register '$routeProvider' as dependency on 'config' method
 myApp.config(['$routeProvider', function($routeProvider) {
 
+    // using '$routeProvider' to declare the routes to our app
+    /* 
+        .when accepts two arguments first one is the url endpoint and the second one is an object which contains the 'templateUrl' and 'controller'
+            templateUrl -> tells which view to go when the url endpoint is visited
+            controller -> tells which controller will control the view
+    
+        .otherwise as a default route when either of the declared routes are not visited.
+            redirectTo -> redirects to a url endpoint
+    */
     $routeProvider
         .when('/home', {
             templateUrl: './views/home.html',
@@ -25,17 +37,21 @@ myApp.run(function() {
     // 
 });
 
+// custom directive to generate random employee
 myApp.directive('randomEmployee', [function () {
 
     /* 
-        E - means element
-        A - means attribute
-        EA - means it can be an attribute or element
+        In custom directives we always return an object, that has the following properties.
+            restrict -> tells how the directive can be used
+                E - means element
+                A - means attribute
+                EA - means it can be an attribute or element
+            scope -> isolated scope to get the scope declared as an attribute on the HTML view, the '=' sign means that it's binding the data together
+            templateUrl -> tells which view to use
+            controller -> this is where you can define the controller for the directive
     */
-
     return {
         restrict: 'E',
-        // isolated scope to get the scope declare as an attribute on the HTML view, the '=' sign means that it's binding the data together
         scope: {
             employees: '=',
             title: '='
@@ -48,8 +64,10 @@ myApp.directive('randomEmployee', [function () {
 
 }]);
 
+// $scope and $http service is a dependency on this controller
 myApp.controller('EmployeeController', ['$scope', '$http', function($scope, $http) {
 
+    // declare the scope objects
     $scope.fullname;
     $scope.position;
     $scope.rate;
@@ -83,6 +101,7 @@ myApp.controller('EmployeeController', ['$scope', '$http', function($scope, $htt
         }
     ];
 
+    // resets the value of the following scope objects
     function clearFields() {
         $scope.fullname = '';
         $scope.position = '';
@@ -91,12 +110,17 @@ myApp.controller('EmployeeController', ['$scope', '$http', function($scope, $htt
         $scope.age = '';
     };
 
+    // removes a selected employee from the employees scope object
     $scope.removeEmployee = function(employee) {
+        // get the index of the selected employee
         let removedEmployeeIndex = $scope.employees.indexOf(employee);
+        // remove the employee from the array using splice method
         $scope.employees.splice(removedEmployeeIndex, 1);
     };
 
+    // add a new employee to the employees scope object
     $scope.addEmployee = () => {
+        // push a new object to the employees scope object
         $scope.employees.push({
             name: $scope.fullname,
             position: $scope.position,
@@ -105,17 +129,24 @@ myApp.controller('EmployeeController', ['$scope', '$http', function($scope, $htt
             age: $scope.age
         });
 
+        // invoke the clearFields function
         clearFields();
+        // show an alert that adding of employee is successful
         alert('Employee successfully added');
     };
 
+    // remove all employee from the employees scope object
     $scope.removeAllEmployee = () => {
+        // ask the user if removing of all employees should proceed or not
         let decision = confirm('Are you sure you want to remove all?');
+
+        // if decision is true then remove all, if false do nothing
         decision ? $scope.employees.length = 0 : ""; 
     };
 
     // console.log(angular.toJson($scope.employees));
 
+    // using the $http service to make a GET api call
     $http.get('https://official-joke-api.appspot.com/jokes/programming/random')
     .then((response) => {
         $scope.jokes = response.data;
